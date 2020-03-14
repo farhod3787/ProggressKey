f0921d8285266321875976332a2b783da9f69b9e
shahzodbekxoltorayev <sh.xoltorayev@gamil.com>
2020-03-14 11:11:17 +0500

    users

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -15,39 +15,7 @@ export class UserComponent implements OnInit {
         $('.sidebar-offcanvas').toggleClass('active')
       });
     });
-    // $(function() {
  
-    //   $(document).on("click", function(e) {
-    //     e.preventDefault();
-    //     var $item = $(".rad-dropmenu-item");
-    //     if ($item.hasClass("active")) {
-    //       $item.removeClass("active");
-    //     }
-    //   });
-       
-    //   $(".rad-toggle-btn").on('click', function() {
-    //     $(".rad-logo-container").toggleClass("rad-nav-min");
-    //     $(".rad-sidebar").toggleClass("rad-nav-min");
-    //     $(".rad-body-wrapper").toggleClass("rad-nav-min");
- 
-    //   });
-    
-    //   $("li.rad-dropdown > a.rad-menu-item").on('click', function(e) {
-    //     e.preventDefault();
-    //     e.stopPropagation();
-    //     $(".rad-dropmenu-item").removeClass("active");
-    //     $(this).next(".rad-dropmenu-item").toggleClass("active");
-    //   });
-    
-    //   $(".fa-chevron-down").on("click", function() {
-    //     var $ele = $(this).parents('.panel-heading');
-    //     $ele.siblings('.panel-footer').toggleClass("rad-collapse");
-    //     $ele.siblings('.panel-body').toggleClass("rad-collapse", function() {
- 
-    //     });
-    //   });
-     
-    // }); 
 
  
 
